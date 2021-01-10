3a210cd361c02564aab19d1dcb6ba52b84d41fd9
田知勲 <j-jihoon@winas.jp>
2021-01-10 23:06:54 +0900

    First Commit
    First Commit

---
+++ GoGakuApp/GoGakuApp/Scene/Home/HomeViewController/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  GoGakuApp
+//
+//  Created by Jh's Macbook Pro on 2021/01/10.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ GoGakuApp/GoGakuApp/Scene/initial/InitialViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InitialViewController.swift
+//  GoGakuApp
+//
+//  Created by Jh's Macbook Pro on 2021/01/10.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
